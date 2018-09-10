008ce7b8caaba1f4aebc999ef397474d5a51c900
Rostislav Zhuravsky <rostislav.zhuravsky@gmail.com>
2018-09-10 23:11:36 +0300

    Update solution.rb

--- codewars/calculator/solution.rb
+++ codewars/calculator/solution.rb
@@ -1,5 +1,10 @@
 class Calculator
   def evaluate(string)
-    Kernel.class_eval(string.gsub(/(\d+)/, '\1' + '.to_f'))
+    %w[+ - * /].each do |op|
+      if string.include?(op)
+        return string.split(op).map { |s| evaluate(s) }.inject(op.strip)
+      end
+    end
+    string.to_f
   end
 end
