2a5650e5a3dab17e35cb088ea368e38674570afc
Alexey Lebedev <joker.leha98@gmail.com>
2018-10-01 14:52:38 +0300

    minor refactoring

--- codewars/roman_numerals_decoder/solution.rb
+++ codewars/roman_numerals_decoder/solution.rb
@@ -14,8 +14,8 @@ def compare_letters(first, second)
 end
 
 def solution(str)
-  letters = str.split(//)
+  letters = str.chars
   letters.each_with_index.inject(0) do |value, pair|
-    value + DECODER[pair.first] * (compare_letters(pair.first, letters[pair.last + 1]) ? 1 : -1)
+    value + DECODER[pair.first] * (compare_letters(pair.first, letters[pair.last.next]) ? 1 : -1)
   end
 end
